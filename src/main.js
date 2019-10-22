99e19a233fbc4eb7cdf677a49cac4c5bf449f395
ShujiOba <shuji365630@gmail.com>
2019-10-22 17:18:47 +0900

    :sparkles: FontAwesomeを追加

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import Vue from 'vue';
+
 import App from './App.vue';
 import router from './router';
 import store from './store';
+
+library.add();
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
