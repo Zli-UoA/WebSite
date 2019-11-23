2b9474051ecd4448c3a4a6439ed9fc31690143db
ShujiOba <shuji365630@gmail.com>
2019-11-23 15:23:04 +0900

    Rename api.js

--- src/js/api.js
+++
@@ -1,7 +0,0 @@
-const getMembers = async () => {
-  const url = 'https://script.google.com/macros/s/AKfycbyifZfvat-lEALV86JrTlPJvQPVlN5CqGurct9mIZKSehnMdSw/exec';
-  const members = await fetch(url).then(x => x.json());
-  return members;
-};
-
-export default { getMembers };
