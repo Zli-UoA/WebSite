5306042a743670ef235f225a267488dd3d5c144d
ShujiOba <shuji365630@gmail.com>
2019-11-25 23:20:39 +0900

    AboutContentを作成

---
+++ src/components/AboutContent.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="about-content-wrapper">
+    <title-tag>About</title-tag>
+    <p>
+      <span>Zliは会津大学のIT系勉強会サークルです。</span>
+      <br />
+      <span>初めは小さなLT会から始まり50人規模の大きなサークルになりました。</span>
+    </p>
+    <div class="content">
+      <about-item
+        v-for="about in abouts"
+        :key="about.title"
+        :image-url="about.imageUrl"
+        :title="about.title"
+      >{{ about.content }}</about-item>
+    </div>
+  </div>
+</template>
+
+<script>
+import TitleTag from './Atom/TitleTag.vue';
+import AboutItem from './AboutItem.vue';
+
+export default {
+  components: { TitleTag, AboutItem },
+  props: {
+    abouts: {
+      type: Array,
+      default: () => [],
+    },
+  },
+};
+</script>
+
+<style scoped>
+.about-content-wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 100px 0;
+}
+
+p {
+  font-size: 32px;
+  text-align: center;
+  margin: 50px 0;
+}
+
+.content {
+  display: flex;
+  width: 100%;
+  padding: 0 128px;
+  box-sizing: border-box;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>
