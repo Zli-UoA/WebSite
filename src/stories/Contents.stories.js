3924b95c574e919f7c9fb8ec0cbc968614979d86
kusaoisii <dkadd911@gmail.com>
2019-12-05 00:14:31 +0900

    headerのstoriesを追加、登録

--- src/stories/Contents.stories.js
+++ src/stories/Contents.stories.js
@@ -6,6 +6,7 @@ import MemberContent from '../components/MemberContent.vue';
 import EventContent from '../components/EventContent.vue';
 import AboutContent from '../components/AboutContent.vue';
 import ContactContent from '../components/ContactContent.vue';
+import HeaderContent from '../components/HeaderContent.vue'
 
 storiesOf('Contents', module)
   .addDecorator(withKnobs)
@@ -151,4 +152,9 @@ storiesOf('Contents', module)
     template: `
       <contact-content :form-url="formUrl" />
     `,
+  }))
+  .add('HeaderContent',()=> ({
+    components: { HeaderContent },
+    template: `<HeaderContent />`
+    ,
   }));
