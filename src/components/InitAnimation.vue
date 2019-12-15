4c599526514a48d13aa4b50cc1f4b878a40ed1b7
ShujiOba <shuji365630@gmail.com>
2019-12-16 04:08:00 +0900

    アニメーションのロゴの色を変更

--- src/components/InitAnimation.vue
+++ src/components/InitAnimation.vue
@@ -1,27 +1,25 @@
 <template>
   <div class="animation-wrapper">
-    <div>
-      <svg
-        width="100vw"
-        height="100vh"
-        viewBox="0 0 649 183"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path class="l" d="M412.253 1H456.88L321.694 150.985H277.262L412.253 1Z" fill="#2C2C2C" />
-        <path
-          class="i"
-          d="M525.891 1H480.318L447.318 38.1117H470.076H492.848L525.891 1Z"
-          fill="#2C2C2C"
-        />
-        <path
-          class="i"
-          d="M347.219 150.985H392.596L483.098 51.1057H437.582L347.219 150.985Z"
-          fill="#2C2C2C"
-        />
-        <path class="z" d="M0.5 16H349.45L213.292 167H648.74" stroke="#FFD600" stroke-width="32" />
-      </svg>
-    </div>
+    <svg
+      width="100vw"
+      height="100vh"
+      viewBox="0 0 649 183"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path class="l" d="M412.253 1H456.88L321.694 150.985H277.262L412.253 1Z" fill="#f2f2f2" />
+      <path
+        class="i"
+        d="M525.891 1H480.318L447.318 38.1117H470.076H492.848L525.891 1Z"
+        fill="#f2f2f2"
+      />
+      <path
+        class="i"
+        d="M347.219 150.985H392.596L483.098 51.1057H437.582L347.219 150.985Z"
+        fill="#f2f2f2"
+      />
+      <path class="z" d="M0.5 16H349.45L213.292 167H648.74" stroke="#ffd600" stroke-width="32" />
+    </svg>
   </div>
 </template>
 
@@ -33,7 +31,7 @@ export default {};
 .animation-wrapper {
   width: 100vw;
   height: 100vh;
-  background-color: var(--color-white);
+  background-color: var(--color-black);
 }
 
 .z {
