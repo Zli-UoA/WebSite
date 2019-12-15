a8f0d697e9ba5aa01707661854115a60898a683b
ShujiOba <shuji365630@gmail.com>
2019-12-15 14:19:11 +0900

    Fix now.json

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-URL=$(now deploy --local-config=./now.json \
-                 --env NODE_ENV=production \
-                 --public \
-                 --no-clipboard \
-                 --token ${ZEIT_TOKEN} \
-                 --scope zli $1)
+URL=$(now deploy \
+  --env NODE_ENV=production \
+  --public \
+  --no-clipboard \
+  --token=${ZEIT_TOKEN} \
+  --scope zli \
+  --local-config=./now.json \
+  $1)
 
-now alias --local-config=./now.json \
-          --token ${ZEIT_TOKEN} \
-          ${URL} \
-          $2
+now alias --local-config=./now.json --token=${ZEIT_TOKEN} ${URL} $2
