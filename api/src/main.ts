6f5cd3954b2158b5d61d691e472fba95b9d270fb
ShujiOba <shuji365630@gmail.com>
2019-12-18 04:14:30 +0900

    CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,11 +4,13 @@ import { Handler } from './handler';
 import { Member, Event, Account } from './Repository';
 import { verifying } from './middleware';
 import Authorizer from './authorizer';
+import microCors from 'micro-cors';
 
 const memberRepo = new Member.FaunaRepository(process.env.FAUNA_SECRET_KEY);
 const eventRepo = new Event.FaunaRepository(process.env.FAUNA_SECRET_KEY);
 const accountRepo = new Account.FaunaRepository(process.env.FAUNA_SECRET_KEY);
 const authorizer = new Authorizer(process.env.SALT, process.env.TOKEN_SECRET, 'sha512', 3);
+const cors = microCors();
 
 const app = router(
   get('/', () => ({ message: 'server is running' })),
@@ -23,6 +25,6 @@ const app = router(
   get('/*', (_, res) => send(res, 404, { message: 'not found' }))
 );
 
-micro(app).listen(3000, () => {
+micro(cors(app)).listen(3000, () => {
   console.log('server is running.');
 });
