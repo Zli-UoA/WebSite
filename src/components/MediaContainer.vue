3f286ec7b02e26e10a1e479adb2dec33c51d5eb7
ShujiOba <shuji365630@gmail.com>
2020-02-04 02:45:29 +0900

    matchMediaを利用するように変更

--- src/components/MediaContainer.vue
+++ src/components/MediaContainer.vue
@@ -1,28 +1,24 @@
 <template>
   <div>
-    <div class="desktop">
-      <slot name="desktop" />
-    </div>
-    <div class="mobile">
-      <slot name="mobile" />
-    </div>
+    <slot v-if="isDesktop" name="desktop" />
+    <slot v-else name="mobile" />
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return { isDesktop: true };
+  },
+  async mounted() {
+    await this.$nextTick();
+    const mql = window.matchMedia("screen and (min-width: 767px)");
+    this.isDesktop = mql.matches;
+    mql.addListener(x => {
+      this.isDesktop = x.matches;
+    });
+  }
+};
 </script>
 
-<style scoped>
-@media only screen and (max-width: 767px) {
-  .desktop {
-    display: none;
-  }
-}
-
-@media screen and (min-width: 767px) {
-  .mobile {
-    display: none;
-  }
-}
-</style>
+<style scoped></style>
