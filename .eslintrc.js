e56c10b0d6cd983f8f7aae815f580f631125198a
ShujiOba <shuji365630@gmail.com>
2020-03-19 16:27:51 +0900

    lintの設定を変えた

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "prettier/prettier": "error"
+    "prettier/prettier": "warn"
   },
   parserOptions: {
     parser: "babel-eslint"
